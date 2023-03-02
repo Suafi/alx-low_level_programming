009fb8d10367404ca7c7edeff3255a4dbeacd2d4
Suafi <gbenasusan@gmail.com>
2023-03-02 08:18:12 -0800

    more pointers

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,7 +3,7 @@
 /**
  * leet - encodes a string into 1337
  * @n: input
- * Retur: the value of n
+ * Return: the value of n
  */
 char *leet(char *n)
 {
@@ -17,4 +17,4 @@ char *leet(char *n)
 		{
 			if (n[i] == s1[j])
 			{
-				n[i] =s2[j];
+				n[i] = s2[j];
